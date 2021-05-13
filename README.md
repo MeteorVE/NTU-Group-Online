a5fe4e5754a8e9fd8b0b322fa92fc6ade5f84980
MeteorVE <ekids12345@gmail.com>
2021-05-13 21:05:16 +0800

    update room create

--- README.md
+++ README.md
@@ -15,6 +15,54 @@ npm run serve
 npm run build
 ```
 
+### Install Other Tools
+```
+npm install -g json-server
+```
+
+### Recommnad Install Some plugin in VSC
+
+(可以在 VSC 右邊 Market 下載)
+
+Search :
+
+- Vetur 
+  - ![](https://i.imgur.com/YzADcQl.png)
+- Eslint
+  - ![](https://i.imgur.com/dtGQufJ.png)
+- Prettier
+  - ![](https://i.imgur.com/2BeSIVt.png) 
+- Copy Relative Path
+- Snippests Suite
+  - 要搜尋 sarah.drasner
+
+#### User Setting
+
+根據上面做一些調整
+```json
+  // I want my default to be 4, but JavaScript/JSON to be 2
+    "editor.tabSize": 4,
+    "[javascript]": {
+        "editor.tabSize": 2
+    },
+    "[json]": {
+        "editor.tabSize": 2
+    },
+
+    // Setting eslint
+    "eslint.validate": [
+        "vue",
+        "html",
+        "javascript"
+    ],
+    
+    // Setting for Vetur's Emmet Function
+    "emmet.includeLanguages": {
+      "vue-html": "html",
+      "vue": "html"
+    },
+```
+
 ### Json Server
 
 ```
--- src/views/RoomCreate.vue
+++ src/views/RoomCreate.vue
@@ -3,51 +3,30 @@
     <h1>Create an Room</h1>
     <form @submit.prevent="createEvent">
       <label>Select a department</label>
-      <select v-model="event.category">
+      <select v-model="room.category">
         <option v-for="dep in department" :key="dep">{{ dep }}</option>
       </select>
+      <select v-model="room.type">
+        <option v-for="rt in roomType" :key="rt">{{ rt }}</option>
+      </select>
 
-      <h3>Name & describe your event</h3>
+      <h3>Name & describe your room</h3>
       <div class="field">
         <label>Title</label>
         <input
-          v-model="event.title"
+          v-model="room.title"
           type="text"
-          placeholder="Add an event title"
+          placeholder="Add an room title"
         />
       </div>
 
       <div class="field">
         <label>Description</label>
         <input
-          v-model="event.description"
+          v-model="room.description"
           type="text"
           placeholder="Add a description"
         />
-      </div>
-
-      <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
-
-      <h3>When is your event?</h3>
-
-      <div class="field">
-        <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
-      </div>
-
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
       </div>
 
       <input type="submit" class="button -fill-gradient" value="Submit" />
@@ -60,8 +39,10 @@ export default {
   components: null,
   data() {
     const department = ['CSIE', 'EE']
+    const roomType = ['public', 'private', 'lesson']
     return {
       department,
+      roomType,
       //categories: this.$store.state.categories,
       room: this.createFreshRoomObject(),
     }
@@ -93,6 +74,7 @@ export default {
         title: '',
         description: '',
         capacity: 10,
+        type: 'public',
       }
     },
   },
