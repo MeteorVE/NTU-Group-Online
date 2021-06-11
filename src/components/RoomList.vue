30edc42a6a1bf29b7d451f3908a375241a40c284
Edith <ritazera12422@gmail.com>
2021-06-11 16:46:16 +0800

    (1)Debug: List can't show in user page.  (2)Delete

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -5,20 +5,14 @@
 
       <el-main id="roomCardContainer" v-if="rooms.length > 0">
         <div
-          v-masonry="roomCardContainer"
-          item-selector=".item"
-          column-width=".item"
+          v-masonry-tile
+          class="item"
+          v-for="(room, index) in rooms"
+          v-bind:key="index"
         >
-          <div
-            v-masonry-tile
-            class="item"
-            v-for="(room, index) in rooms"
-            v-bind:key="index"
-          >
-            <RoomListCard :room="room" />
-          </div>
+          <RoomListCard :room="room" />
         </div>
-      </el-main>
+    </el-main>
     </el-container>
   </div>
 </template>
@@ -26,7 +20,7 @@
 <script>
 import RoomListCard from '@/components/RoomListCard.vue'
 import UserService from '@/services/UserService.js'
-import { ElMessage } from 'element-plus'
+// import { ElMessage } from 'element-plus'
 
 export default {
   components: {
@@ -40,70 +34,48 @@ export default {
   },
 
   created() {
-    if (this.$store.state.token) {
-      this.$store
-        .dispatch('refreshToken', this.event)
-        .then((resRefresh) => {
-          console.log(resRefresh)
-          'status' in resRefresh &&
-            console.log(
-              '[Home.vue.created.then] refreshToken then：' +
-                '\nstatus code: ' +
-                resRefresh.status +
-                '\naccess token: ' +
-                JSON.stringify(resRefresh.data.access)
-            )
+    UserService.getUserRoom()
+      .then((response) => {
+        this.rooms = response.data
+        console.log("aaaaaaaaa", response.data)
+      })
+      .catch((error) => {
+        console.log(
+          '[Home.vue] error: created(), RoomService.getRooms():',
+          '\n理論上這邊不會再有錯誤了，注意一下\n',
+          error.response
+        )
+      })
+        
+      .catch((err) => {
+        console.log(
+          '[Home.vue.created.catch], token exist but:',
+          '\nstatus code: ',
+          err.response.status,
+          '\nerror message: ',
+          JSON.stringify(err.response.data)
+        )
+        if (
+          'code' in err.response.data &&
+          err.response.data['code'] == 'token_not_valid'
+        ) {
+          console.log('[Home.vue.created.catch] delete token')
+          this.$store.dispatch('resetToken')
+        }
+        this.$router.push({
+          name: 'login',
         })
-        .then(() => {
-          UserService.getUserRoom()
-            .then((response) => {
-              this.rooms = response.data
-              console.log("aaaaaaaaa", response.data)
-            })
-            .catch((error) => {
-              console.log(
-                '[Home.vue] error: created(), RoomService.getRooms():',
-                '\n理論上這邊不會再有錯誤了，注意一下\n',
-                error.response
-              )
-            })
-        })
-        .catch((err) => {
-          console.log(
-            '[Home.vue.created.catch], token exist but:',
-            '\nstatus code: ',
-            err.response.status,
-            '\nerror message: ',
-            JSON.stringify(err.response.data)
-          )
-          if (
-            'code' in err.response.data &&
-            err.response.data['code'] == 'token_not_valid'
-          ) {
-            console.log('[Home.vue.created.catch] delete token')
-            this.$store.dispatch('resetToken')
-          }
-          this.$router.push({
-            name: 'login',
-          })
-        })
-    } else {
-      console.log('plz login !')
-      ElMessage.error('請登入 !')
-      // window.location.href = '/login'
-      this.$router.push({
-        name: 'login',
       })
     }
-  },
+  
 }
 </script>
 
 <style scoped>
 
-.item {
+/* .item {
   width: 100%;
-}
+} */
 
 
 </style>
